f72c93c0e2b25002034b28dd888dc406173fd4b8
Ashish47633 <ashishvishwakarma7529@gmail.com>
2024-12-18 22:03:57 +0530

    First V1

---
+++ src/main/java/com/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.api.exception;
+
+import com.api.payload.ErrorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDto> userNotFound(
+            UserNotFoundException u,
+            WebRequest request
+    ){
+        ErrorDto error =  new ErrorDto(u.getMessage(), new Date(), request.getDescription(true) );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
